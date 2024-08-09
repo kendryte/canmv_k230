9631199546a0fea52e4645bcf5d17f1834e41c44
kendryte747 <kendryte747@gmail.com>
2024-08-09 18:46:49 +0800

    Update

--- src/rtsmart/libnncase.mk
+++ src/rtsmart/libnncase.mk
@@ -1,8 +1,8 @@
 
 nncase_inc_dir :=
-nncase_inc_dir += $(SDK_RTSMART_SRC_DIR)/nncase/riscv64
-nncase_inc_dir += $(SDK_RTSMART_SRC_DIR)/nncase/riscv64/nncase/include
+nncase_inc_dir += $(SDK_RTSMART_SRC_DIR)/lib/nncase/riscv64
+nncase_inc_dir += $(SDK_RTSMART_SRC_DIR)/lib/nncase/riscv64/nncase/include
 
 NNCASE_INC = $(addprefix -I, $(nncase_inc_dir))
 NNCASE_LIBS = -lNncase.Runtime.Native -lnncase.rt_modules.k230 -lfunctional_k230 -lstdc++
-NNCASE_LIB_DIR = -L$(SDK_RTSMART_SRC_DIR)/nncase/riscv64/nncase/lib
+NNCASE_LIB_DIR = -L$(SDK_RTSMART_SRC_DIR)/lib/nncase/riscv64/nncase/lib

3cf4e9be379049243cb682513c98508c745fe47d
lijian528 <lijian01@canaan-creative.com>
2025-09-05 17:29:27 +0800

    [i2c_slave.py] bug fix. ( Copy from CanMV I2C Slave API )

--- resources/examples/03-Machine/i2c_slave.py
+++ resources/examples/03-Machine/i2c_slave.py
@@ -1,43 +1,40 @@
-from machine import Pin
-from machine import FPIOA
-from machine import I2C_Slave
-import time,os
+from machine import Pin, FPIOA, I2C_Slave
+import time
+import os
+
+# 实例化 FPIOA
+fpioa = FPIOA()
+# 设置 Pin14 为 GPIO输出模式，用于通知主机有数据更新，初始化为输出高电平
+fpioa.set_function(14, FPIOA.GPIO14)
+pin = Pin(14, Pin.OUT, pull=Pin.PULL_UP, drive=7)
+pin.value(1)
+
+# 设置I2C2的iomux，gpio11为SCL，gpio12为SDA
+fpioa.set_function(11, FPIOA.IIC2_SCL, pu = 1)
+fpioa.set_function(12, FPIOA.IIC2_SDA, pu = 1)
 
 
-#初始化gpio用于通知主机有数据更新
-def gpio_int_init():
-    # 实例化FPIOA
-    fpioa = FPIOA()
-    # 设置Pin5为GPIO5
-    fpioa.set_function(5, FPIOA.GPIO5)
-    # 实例化Pin5为输出
-    pin = Pin(5, Pin.OUT, pull=Pin.PULL_NONE, drive=7)
-    # 设置输出为高
-    pin.value(1)
-    return pin
-
-pin = gpio_int_init()
-#i2c slave设备id列表
+# I2C 从设备 ID 列表
 device_id = I2C_Slave.list()
-print("Find i2c slave device:",device_id)
+print("Find I2C slave device:", device_id)
 
-#构造I2C Slave，设备地址为0x01，模拟eeprom映射的内存大小为20byte
-i2c_slave = I2C_Slave(device_id[0],addr=0x10,mem_size=20)
+# 构造 I2C 从设备，设备地址为 0x10，模拟 EEPROM 映射的内存大小为 20 字节
+i2c_slave = I2C_Slave(device_id[0], addr=0x10, mem_size=20)
 
-#等待master发送数据，并读取映射内存中的值
-last_dat = i2c_slave.readfrom_mem(0,20)
+# 等待主设备发送数据，并读取映射内存中的值
+last_dat = i2c_slave.readfrom_mem(0, 20)
 dat = last_dat
 while dat == last_dat:
-    dat = i2c_slave.readfrom_mem(0,20)
-    time.sleep_ms(100)
+    dat = i2c_slave.readfrom_mem(0, 20)
+    time.sleep_ms(1)
     os.exitpoint()
-print(dat)
-    
-#修改映射内存中的值
-for i in range(len(dat)):
-    dat[i] = i
-i2c_slave.writeto_mem(0,dat)
-#通知主机有数据更新
+
+# 等待总线数据传输完毕
+time.sleep_ms(100)
+
+print(dat.hex())
+
+i2c_slave.writeto_mem(0, dat)
+# 通知主机数据已经写入成功
 pin.value(0)
-time.sleep_ms(1)
-pin.value(1)
+time.sleep_ms(1000)

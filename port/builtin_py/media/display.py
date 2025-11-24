0aa78623a0bfbea33b6d02bc83bacad1d7044236
kendryte747 <kendryte747@gmail.com>
2025-11-24 16:17:45 +0800

    feat(display): add support for ST7102 display panel

--- port/builtin_py/media/display.py
+++ port/builtin_py/media/display.py
@@ -17,6 +17,7 @@ class Display:
     NT35516         = const(307)
     NT35532         = const(308)
     GC9503          = const(309)
+    ST7102          = const(310)
 
     # define VO channel
     LAYER_VIDEO1 = K_VO_DISPLAY_CHN_ID1
@@ -363,6 +364,22 @@ class Display:
                     cls._connector_type = GC9503_MIPI_2LAN_480X800_60FPS
                 else:
                     raise ValueError(f"GC9503 unsupport {_width}x{_height}")
+
+                _width = None
+                _height = None
+                _flag = None
+            elif _type == Display.ST7102:
+                _width = width if width is not None else 640
+                _height = height if height is not None else 480
+                _flag = flag if flag is not None else Display.FLAG_ROTATION_90
+
+                if _width == 640 and _height == 480:
+                    cls._panel_flag = _flag
+                    cls._connector_type = ST7102_MIPI_2LAN_480X640_60FPS
+                elif _width == 480 and _height == 640:
+                    cls._connector_type = ST7102_MIPI_2LAN_480X640_60FPS
+                else:
+                    raise ValueError(f"ST7102 unsupport {_width}x{_height}")
 
                 _width = None
                 _height = None
